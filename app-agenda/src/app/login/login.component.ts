00ab637e812e300fca9ec87c4c6e50fddcdaf79d
thiago.rasquino@7comm.com.br <thiago.programador.net@gmail.com>
2020-06-16 13:01:47 -0300

    Validação simples do login

--- app-agenda/src/app/login/login.component.ts
+++ app-agenda/src/app/login/login.component.ts
@@ -10,18 +10,27 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   telaLogin: boolean = false;
-
   usuario: Usuario = new Usuario();
-
+  usuarioMockado: Usuario = new Usuario();
+  
   constructor(private router: Router ) { }
-
+    
   ngOnInit() {
   }
 
   fazerLogin(){
-    console.log(this.usuario);
-    this.router.navigate(['/home']);
-    this.telaLogin = false;
+    //deve ser obtido pelo serviço
+    this.usuarioMockado.nome = "usuario";
+    this.usuarioMockado.senha = "123";
+    
+    console.log("Nome: " + this.usuario.nome + " - Senha: " + this.usuario.senha);
+
+    if(this.usuario.nome == this.usuarioMockado.nome && this.usuario.senha == this.usuarioMockado.senha){
+      this.router.navigate(['/home']);
+      this.telaLogin = false;
+    }else{
+      alert("Acesso negado.")
+    }
   }
 
 
